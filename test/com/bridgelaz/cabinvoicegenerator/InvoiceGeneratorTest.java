c6cce9a1599fbe3700192e08ee882a07a8526528
mahidhar reddy <mahidhar100008@gmail.com>
2023-12-17 16:51:28 +0530

    Added : usecase 1 , test cases are added for usecase 1

---
+++ test/com/bridgelaz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -0,0 +1,52 @@
+
+package com.bridgelaz.cabinvoicegenerator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+/*
+ * Test class for InvoiceGenerator that includes various test cases.
+ */
+
+public class InvoiceGeneratorTest {
+
+    private InvoiceGenerator invoiceGenerator;
+
+    /*
+     * Setup method to initialize the InvoiceGenerator before each test case.
+     */
+    @Before
+    public void setup(){
+        invoiceGenerator = new InvoiceGenerator(10 , 1 , 5);
+    }
+
+    /*
+     * Test case for calculating fare based on distance and time.
+     */
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare(){
+
+        double distance = 2;
+        int time = 5;
+        double fair = invoiceGenerator.calculateFare(distance , time);
+        assertEquals(25 , fair , 0.0001);
+    }
+
+    /*
+     * Test case for calculating minimum fare when distance and time are less.
+     */
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare(){
+        double distance = 0.2;
+        int time = 1;
+        double fair = invoiceGenerator.calculateFare(distance , time);
+        assertEquals(5 , fair , 0.0);
+    }
+
+
+}
